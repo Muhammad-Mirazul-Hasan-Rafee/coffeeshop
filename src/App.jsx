32843a14f7f3ff90140a3a8b4d766953d831ae58
Rafee <rafeehasan88@gmail.com>
2025-05-18 11:22:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import CoffeeCard from './Components/CoffeeCard';
+
+
+function App() {
+ 
+const coffees = useLoaderData();
+
+  return (
+    <div className='m-20'>
+     
+     
+
+      
+     
+    </div>
+  )
+}
+
+export default App;
